c5617d160c0b8f58da441b3dbc270673a2719861
Luiz Normanha <luiznormanha@gmail.com>
2025-02-24 10:30:21 -0300

    feat: extra colors

--- src/components/custom/ColorPicker/index.tsx
+++ src/components/custom/ColorPicker/index.tsx
@@ -15,6 +15,10 @@ const colors = [
   "bg-pink-500",
   "bg-indigo-500",
   "bg-gray-500",
+  "bg-orange-500",
+  "bg-teal-500",
+  "bg-fuchsia-500",
+  "bg-cyan-500",
 ];
 
 interface ColorPickerProps {
